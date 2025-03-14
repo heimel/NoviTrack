68efaacd666813a68538de62fe8fceba9f01b713
Alexander Heimel <alexander.heimel@gmail.com>
2025-03-14 13:26:30 +0100

    Created new generic picam slave to use for behavior_arena

--- nt_create_session.m
+++ nt_create_session.m
@@ -3,7 +3,14 @@ function [json, session_path] = nt_create_session()
 %
 %  [JSON, SESSIONPATH] = nt_create_session()
 %
-% 2023, Alexander Heimel
+%      Ask for session details in interaction with FYD database.
+%      Upon close, an acqReady file is created in
+%      params.root_communication_path/SETUP with the session path.
+%
+%    For Raspberry Pi video recording, use cli_picam_slave.py SETUP on
+%    the pi. 
+%
+% 2023-2025, Alexander Heimel
 
 
 params = nt_default_parameters();
@@ -13,35 +20,35 @@ if isempty(json)
     logmsg('Failed to get session data. Quitting.');
     return
 end
-[json, session_path] = createSessionFolder( json, params );
+[json, session_path] = create_session_folder( json, params );
 savejson('', json, fullfile(session_path,[json.sessionid '_session.json']));
 
 
 % Communicate
 communication_path = fullfile(params.root_communication_path,json.setup);
+if ~exist(communication_path,'dir')
+    errormsg(['Folder ' communication_path ' does not yet exist. Check setup name and params.root_communication_path in processparams_local.m or create folder first. Not writing acqready.'],false);
+    return
+end
+
 acqready_filename = fullfile(communication_path,'acqReady');
 [fid,message] = fopen(acqready_filename,'wt');
 if fid == -1
-    errormsg(['Cannot write acqReady. ' message])
+    errormsg(['Cannot write acqReady ' acqready_filename '. ' message])
 end
 fprintf(fid,'pathSpec\n%s\n',session_path);
 fclose(fid);
 logmsg(['Wrote sessionpath in ' acqready_filename ]);
 
-
-
 end
 
 
-function  [json, sessionpath] = createSessionFolder( json, params )
+function  [json, sessionpath] = create_session_folder( json, params )
 % Create session folder and increase session number if necessary
 new_folder = false;
 existing_folder = false;
 while ~new_folder
     json.sessionid = [json.subject '_' json.date(json.date~='-') '_' sprintf('%03d', json.sessnr)];
-    % sessionpath = fullfile(params.networkpathbase, ...
-    %     json.project, 'Data_collection', json.dataset, json.subject , json.sessionid);
-
     sessionpath = nt_session_path(json,params);
 
     if ~exist(sessionpath, 'dir')
